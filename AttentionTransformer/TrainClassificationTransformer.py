c1edce1e8bf2f0a78929f211c791dece00b0cafd
vatsals <vatsals@quinnox.com>
2020-06-11 11:55:10 +0530

    fixed model save after every

--- AttentionTransformer/TrainClassificationTransformer.py
+++ AttentionTransformer/TrainClassificationTransformer.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import os
+from tqdm import tqdm, tqdm_notebook
 
 from .utilities import device
 
@@ -20,7 +21,7 @@ def classification_performance(pred, true):
     return loss, corrects, total
 
 
-def fit_classification(epoch, dataloader, model, pad_id, optimizer, pbar, save_every = None, save_path = None, phase = 'training', clip = 2):
+def fit_classification(epoch, dataloader, model, pad_id, optimizer, pbar, save_every = None, save_path = None, phase = 'training', clip = 2, notebook = False):
 
     if phase == 'training':
 
@@ -32,6 +33,11 @@ def fit_classification(epoch, dataloader, model, pad_id, optimizer, pbar, save_e
 
     total_loss, n_items_total, n_items_correct = 0, 0, 0
 
+
+    if notebook:
+        progress = tqdm_notebook(total = len(dataloader), leave = False, desc = "Dataloader")
+    else:
+        progress = tqdm(total = len(dataloader), leave = False, desc = "Dataloader")
 
     for ix, batch in enumerate(dataloader):
 
@@ -55,28 +61,31 @@ def fit_classification(epoch, dataloader, model, pad_id, optimizer, pbar, save_e
         n_items_correct += corrects
         total_loss += loss.item()
 
+        progress.update()
+
     loss_per_item = total_loss / n_items_total
     accuracy = n_items_correct / n_items_total
 
     t = f'''{phase.upper} EPOCH: {epoch} | Loss: {loss_per_item} | Accuracy: {accuracy}'''
 
     if save_every:
-        if not save_path:
-            return f'Got results {t}, please provide a path to `save_path` argument to save your model after every {save_every} epochs as chosen'
-        if not os.path.exists(save_path):
-            os.mkdir(save_path)
-        
-        save_path_ = os.path.join(save_path, f'classification_transformer_training_epoch_{epoch}.pt')
-        save_path_dict = os.path.join(save_path, f'classification_transformer_training_epoch_{epoch}_state_dict.pth')
+        if epoch % save_every == 0:
+            if not save_path:
+                return f'Got results {t}, please provide a path to `save_path` argument to save your model after every {save_every} epochs as chosen'
+            if not os.path.exists(save_path):
+                os.mkdir(save_path)
+            
+            save_path_ = os.path.join(save_path, f'classification_transformer_training_epoch_{epoch}.pt')
+            save_path_dict = os.path.join(save_path, f'classification_transformer_training_epoch_{epoch}_state_dict.pth')
 
-        torch.save(model, save_path_)
-        torch.save({
-            'epoch': epoch,
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'loss': loss_per_item,
-            'acc': accuracy
-        }, save_path_dict)
+            torch.save(model, save_path_)
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': loss_per_item,
+                'acc': accuracy
+            }, save_path_dict)
 
     pbar.write(t)
     pbar.update(1)
